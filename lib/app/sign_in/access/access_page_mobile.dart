0b5878474c02ecb348793dc85e80b991ca01ab07
Verónica <bc.veros@gmail.com>
2023-02-21 12:49:52 -0500

    solving bug on login

--- lib/app/sign_in/access/access_page_mobile.dart
+++ lib/app/sign_in/access/access_page_mobile.dart
@@ -3,6 +3,7 @@ import 'package:enreda_app/utils/const.dart';
 import 'package:enreda_app/values/values.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../../../common_widgets/show_back_icon.dart';
 import '../../../common_widgets/spaces.dart';
 import '../../../common_widgets/stores_buttons.dart';
 import '../../../values/strings.dart';
@@ -17,29 +18,10 @@ class _AccessPageMobileState extends State<AccessPageMobile> {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size(double.infinity, 60),
-          child: AppBar(
-            title: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Image.asset(
-                    ImagePath.LOGO,
-                    height: 25.0,
-                  ),
-                ],
-              ),
-            ),
-            backgroundColor: Constants.white,
-            elevation: 1.0,
-          ),
-        ),
-        backgroundColor: Constants.white,
+      backgroundColor: Constants.white,
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SpaceH20(),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Text(
@@ -51,7 +33,7 @@ class _AccessPageMobileState extends State<AccessPageMobile> {
                 ),
               ),
             ),
-            SpaceH30(),
+            SpaceH20(),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: EmailSignInFormChangeNotifier.create(context),

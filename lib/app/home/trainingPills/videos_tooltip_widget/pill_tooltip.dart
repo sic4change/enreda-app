203a222f0ddca3eeaa8ba4de286e164c898db056
Helio Ojeda Reyes <hojeda@sic4change.org>
2024-07-25 12:03:01 +0100

    Remove Pill Tooltip overlay when clicking outside it

--- lib/app/home/trainingPills/videos_tooltip_widget/pill_tooltip.dart
+++ lib/app/home/trainingPills/videos_tooltip_widget/pill_tooltip.dart
@@ -41,64 +41,73 @@ class _PillTooltipState extends State<PillTooltip> {
       if (x + 300 > MediaQuery.of(context).size.width) x -= 260;
       if (y + 300 > MediaQuery.of(context).size.height) y -= 180;
 
-      return Positioned(
-        // Position the tooltip relative to the widget
-        top: Responsive.isMobile(context)? (MediaQuery.of(context).size.height * 0.5) - 150
-          : y - 20,
-        /*top: Responsive.isMobile(context) ? 100
-            : Responsive.isTablet(context) || Responsive.isDesktopS(context) ? 200
-                : MediaQuery.of(context).size.height * 0.32,*/
-        left: Responsive.isMobile(context)? (MediaQuery.of(context).size.width * 0.5) - 150
-            : x - 20,
-        /*right: x - 20,Responsive.isMobile(context) ? 20
-            : Responsive.isTablet(context) || Responsive.isDesktopS(context) ? 50
-                : MediaQuery.of(context).size.width * 0.25,*/
-        child: Card(
-          color: Colors.transparent,
-          elevation: 0,
-          child: Stack(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(10.0),
-                padding: const EdgeInsets.only(top: 13.0, bottom: 30),
-                decoration: BoxDecoration(
-                  color: AppColors.primary900,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 300,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: CustomTextTitle(
-                        title: widget.title,
-                        color: AppColors.white,
-                      ),
+      // Remove the Stack and GestureDetector if we don't want to ignore the first click of the user outside the overlay entry (and remove the overlay in the dispose())
+      return Stack(
+        children: [
+          GestureDetector(onTap: () => _overlayEntry.remove(),),
+          Positioned(
+            // Position the tooltip relative to the widget
+            top: Responsive.isMobile(context)? (MediaQuery.of(context).size.height * 0.5) - 150
+              : y - 20,
+            /*top: Responsive.isMobile(context) ? 100
+                : Responsive.isTablet(context) || Responsive.isDesktopS(context) ? 200
+                    : MediaQuery.of(context).size.height * 0.32,*/
+            left: Responsive.isMobile(context)? (MediaQuery.of(context).size.width * 0.5) - 150
+                : x - 20,
+            /*right: x - 20,Responsive.isMobile(context) ? 20
+                : Responsive.isTablet(context) || Responsive.isDesktopS(context) ? 50
+                    : MediaQuery.of(context).size.width * 0.25,*/
+            child: Card(
+              color: Colors.transparent,
+              elevation: 0,
+              child: Stack(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.only(top: 13.0, bottom: 30),
+                    decoration: BoxDecoration(
+                      color: AppColors.primary900,
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    _buildTrainingTooltipVideo(context),
-                  ],
-                ),
+                    width: 300,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: CustomTextTitle(
+                            title: widget.title,
+                            color: AppColors.white,
+                          ),
+                        ),
+                        _buildTrainingTooltipVideo(context),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 10,
+                    right: 10,
+                    child: IconButton(
+                      hoverColor: Colors.transparent,
+                        onPressed: () {
+                          _overlayEntry.remove();
+                        },
+                        icon: Icon(Icons.close), iconSize: 20,
+                    ),
+                  ),
+                ],
               ),
-              Positioned(
-                top: 10,
-                right: 10,
-                child: IconButton(
-                  hoverColor: Colors.transparent,
-                    onPressed: () {
-                      _overlayEntry.remove();
-                    },
-                    icon: Icon(Icons.close), iconSize: 20,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       );
     });
   }
 
   @override
   void dispose() {
+    /*if (_overlayEntry.mounted) {
+      _overlayEntry.remove();
+    }*/
     _overlayEntry.dispose();
     super.dispose();
   }

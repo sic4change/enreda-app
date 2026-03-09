2cbde375a1887fb0cebbe1fff7aff0af602c93a6
Vivek Mirpuri <vivek.mirp@gmail.com>
2026-03-09 14:52:20 +0000

    feat: update FORM_NEWSLETTER string to reflect new communication consent.

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -452,7 +452,7 @@ class StringConst {
   static const String FORM_SUCCESS = "¡Registro exitoso!";
   static const String FORM_SUCCESS_MAIL = "Por favor revisa tu correo electrónico.";
   static const String FORM_ACCEPT = "Aceptar";
-  static const String FORM_NEWSLETTER = "Acepto suscribirme a la newsletter con fines de promoción";
+  static const String FORM_NEWSLETTER = "Acepto recibir información y comunicaciones por parte de SIC4Change";
   static const String FORM_ERROR= "Error al crea la cuenta";
   static const String FORM_SELECT= "Seleccionar";
   static const String FORM_SELECT_AT_LEAST_ONE = 'Debes seleccionar al menos un interés';

554521cd07e63b93d1c18d380654fd4dab77f258
Juan Lucena <jlucena@sic4change.org>
2026-08-18 13:37:40 +0200

    feat: update SDK requirements, improve resource detail UI responsiveness, and initialize native Swift integration files.

---
+++ ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -0,0 +1,60 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Generated file. Do not edit.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "FlutterGeneratedPluginSwiftPackage",
+    platforms: [
+        .iOS("15.0")
+    ],
+    products: [
+        .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
+    ],
+    dependencies: [
+        .package(name: "cloud_firestore", path: "../.packages/cloud_firestore-6.3.0"),
+        .package(name: "file_picker", path: "../.packages/file_picker-10.3.10"),
+        .package(name: "firebase_analytics", path: "../.packages/firebase_analytics-12.3.0"),
+        .package(name: "firebase_auth", path: "../.packages/firebase_auth-6.4.0"),
+        .package(name: "firebase_core", path: "../.packages/firebase_core-4.7.0"),
+        .package(name: "firebase_messaging", path: "../.packages/firebase_messaging-16.2.0"),
+        .package(name: "firebase_storage", path: "../.packages/firebase_storage-13.3.0"),
+        .package(name: "flutter_pdfview", path: "../.packages/flutter_pdfview-1.4.4"),
+        .package(name: "google_sign_in_ios", path: "../.packages/google_sign_in_ios-5.9.0"),
+        .package(name: "image_cropper", path: "../.packages/image_cropper-11.0.0"),
+        .package(name: "image_picker_ios", path: "../.packages/image_picker_ios-0.8.13+6"),
+        .package(name: "share_plus", path: "../.packages/share_plus-12.0.2"),
+        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
+        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.2"),
+        .package(name: "url_launcher_ios", path: "../.packages/url_launcher_ios-6.4.1"),
+        .package(name: "webview_flutter_wkwebview", path: "../.packages/webview_flutter_wkwebview-3.24.5"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "cloud-firestore", package: "cloud_firestore"),
+                .product(name: "file-picker", package: "file_picker"),
+                .product(name: "firebase-analytics", package: "firebase_analytics"),
+                .product(name: "firebase-auth", package: "firebase_auth"),
+                .product(name: "firebase-core", package: "firebase_core"),
+                .product(name: "firebase-messaging", package: "firebase_messaging"),
+                .product(name: "firebase-storage", package: "firebase_storage"),
+                .product(name: "flutter-pdfview", package: "flutter_pdfview"),
+                .product(name: "google-sign-in-ios", package: "google_sign_in_ios"),
+                .product(name: "image-cropper", package: "image_cropper"),
+                .product(name: "image-picker-ios", package: "image_picker_ios"),
+                .product(name: "share-plus", package: "share_plus"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
+                .product(name: "url-launcher-ios", package: "url_launcher_ios"),
+                .product(name: "webview-flutter-wkwebview", package: "webview_flutter_wkwebview"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
+        )
+    ]
+)
